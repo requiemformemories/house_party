6b41840003cedaa76363fb0b6062f36026b3ef83
egil.huang <egil.huang@gigabyte.com>
2016-10-01 16:44:37 +0800

    Player view will rortate according to gyrscope of android phone.

--- Assets/Script/EJ_MotionMonitor.cs
+++ Assets/Script/EJ_MotionMonitor.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class EJ_MotionMonitor : MonoBehaviour {
 
+	
+
+
 	// Use this for initialization
 	void Start () {
-	
+		Input.gyro.enabled = true;
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame 
 	void Update () {
-	
+		//Rotating a camera using the gyroscope
+		//根據玩家的手機的轉向移動視角
+		transform.Rotate (Input.gyro.rotationRateUnbiased.x, 
+			-Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRate.z);
 	}
 }
