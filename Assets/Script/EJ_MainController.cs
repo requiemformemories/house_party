98461fcc340ee100e9695f633638d87f2da7a92d
egil.huang <egil.huang@gigabyte.com>
2016-12-07 00:18:12 +0800

    1.[Body Motion Mode] Use DeltaTime mechanism to avoid twitching effect. 2.Enable body motion mode. --- 1.[體感模式] 用補間的方式取代直接位移以解決抖動的問題 2.啟用體感模式

--- Assets/Script/EJ_MainController.cs
+++ Assets/Script/EJ_MainController.cs
@@ -8,19 +8,22 @@ using System.Collections;
 
 public class EJ_MainController : MonoBehaviour {
 	//
-	private const float UpperBoundY = 1.2f;
+	private const float UpperBoundY = -0.95f;
 	//
-	private const float LowerBoundY = -4.0f; 
+	private const float LowerBoundY = -8.50f; 
 	//
-	private const float f_isDown_threshold = 0.48f;
+	private const float f_isDown_threshold = 0.25f;
 	//
-	private const float speed = 5.0f;
+	private const float speedup = 3.0f;
 	//
 	private const float UpperBoundX = 0.35f;
 	//
 	private const float LowerBoundX = -0.35f;
 	//位移至少要超過這個值才不會被視為是抖動，細微抖動不處理
 	private const float twitchingThreshold = 0.01f;
+
+	private const float gravZ_UpperBound = 0.95f;
+	private const float gravZ_LowerBound = 0.05f;
 
 	//private float gravX; // X方向的重力分量 (本情境中不使用)
 	private float gravZ; // Z方向的重力分量 (本情境中將之轉換為Y方向位移)
@@ -42,42 +45,38 @@ public class EJ_MainController : MonoBehaviour {
 	// Update is called once per frame
 	void Update()
 	{
+		gravZ = Input.acceleration.z;
 
-		gravZ = Input.acceleration.y;
+		//改成每偵判斷isDown布林值，實機測試前要啟用(去掉註解)
+		if (gravZ >= f_isDown_threshold)
+			isDown = true;
+		else
+			isDown = false;
 
-        //改成每偵判斷isDown布林值，實機測試前要啟用(去掉註解)
-        if (gravZ >= 0.48)
-            isDown = true;
-        else
-            isDown = false;
+		if (isEnable) {
 
-        if (isEnable)
-        {
-
-			if (gravZ < 0.10)
+			if (gravZ < gravZ_LowerBound)
 				fYShift = -transform.position.y + LowerBoundY;
-			else if (gravZ >= 0.95)
+			else if (gravZ >= gravZ_UpperBound)
 				fYShift = -transform.position.y + UpperBoundY;
 			else
 				fYShift = -transform.position.y + (UpperBoundY - LowerBoundY) *
-				(float)(gravZ - (float)0.10) / (float)0.85 + LowerBoundY;
-        }
+					(float)(gravZ - gravZ_LowerBound) / (float)(gravZ_UpperBound-gravZ_LowerBound) + LowerBoundY;
+		}
 
-        //		gravX = Input.acceleration.x;
-        //		if (gravX < -0.85)
-        //			fXShift = -transform.position.x + LowerBoundX;
-        //		else if (gravX >= 0.85)
-        //			fXShift = -transform.position.x + UpperBoundX;
-        //		else
-        //			fXShift = -transform.position.x + (UpperBoundX - LowerBoundX) *
-        //				(float)(gravX + (float)0.85) / (float)1.7 + LowerBoundX;
+		//		gravX = Input.acceleration.x;
+		//		if (gravX < -0.85)
+		//			fXShift = -transform.position.x + LowerBoundX;
+		//		else if (gravX >= 0.85)
+		//			fXShift = -transform.position.x + UpperBoundX;
+		//		else
+		//			fXShift = -transform.position.x + (UpperBoundX - LowerBoundX) *
+		//				(float)(gravX + (float)0.85) / (float)1.7 + LowerBoundX;
 
-		if (isEnable && fYShift >= twitchingThreshold) 
-		{
-			//float step = speed * Time.deltaTime;
-			Vector3 _v3 = new Vector3 (0, transform.position.y + fYShift, 0);
+		if (isEnable && Mathf.Abs(fYShift) >= twitchingThreshold) {
+			Vector3 _v3 = new Vector3 (0, fYShift, 0);
 			//transform.Translate (0, fYShift, 0);
-			transform.Translate (_v3 * Time.deltaTime * fYShift / Time.deltaTime, Space.World);
+			transform.Translate (_v3 * Time.deltaTime * speedup);
 		}
 	}
 
@@ -93,7 +92,7 @@ public class EJ_MainController : MonoBehaviour {
 
 	void OnGUI(){  
 		//把抓取到的X,Y,Z軸分量放到畫面上  
-		GUI.Label(new Rect(100,100,300,300),"x="+Input.acceleration.x+"   y="+Input.acceleration.y+"   z="+Input.acceleration.z);  
-  
+		GUI.Label(new Rect(100,100,300,300),"x="+Input.acceleration.x+"   y="+Input.acceleration.y+"   z="+Input.acceleration.z); 
+		//GUI.Label(new Rect(100,100,300,300)," Y:  " + transform.position.y); 
 	}  
 }
